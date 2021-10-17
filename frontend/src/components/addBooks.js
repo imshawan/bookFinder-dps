29b576deabefa072b784c8c3e78a05c18a406fad
Shawan Mandal <shawan.sm049@gmail.com>
2021-10-17 19:37:19 +0530

    Improved view of AddBooks page

--- frontend/src/components/addBooks.js
+++ frontend/src/components/addBooks.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { useForm } from 'react-hook-form';
 import { useEffect, useState } from "react";
+import Cancel from '../pngs/cancel.png'
+import Checked from '../pngs/checked.png'
 
 const AddBooks = () => {
     const [Response, setResponse] = useState(false);
     const [isLoading, setisLoading] = useState(true);
+    const [respStatus, setrespStatus] = useState('');
 
     const {
     register,
@@ -42,6 +45,7 @@ const AddBooks = () => {
             setResponse(true)
             setisLoading(false)
         } else {
+            if (resp.message) {setrespStatus(resp.message)}
             setResponse(false)
             setisLoading(false)
         }
@@ -51,16 +55,16 @@ const AddBooks = () => {
     return (
       <div>
            <header className="w3-container w3-red w3-center" style={{padding:'40px 16px'}}>
-                <h2 className="w3-margin w3-jumbo">Add Books.</h2>
+                <h3 className="w3-margin w3-jumbo">Add Books.</h3>
             </header>
-            <div id="main-content" className="main-content">
+            <div id="main-content" className="main-content mt-5">
                 <div className="add-books" style={{display:`${isLoading ? '' : 'none'}`}}>
-                    <form className="action-form" onSubmit={handleSubmit(onSubmit1)}>
+                    <form className="action-form mt-5" onSubmit={handleSubmit(onSubmit1)}>
                         <input className="input-content" type="text" placeholder="Enter Title:" id="title" {...register('title', { required: true })} /> {/* register an input */}
                         <input className="input-content" type="text" placeholder="Enter Author:" id="author" {...register('author', { required: true })} />
                         <input className="input-content" type="text" placeholder="Enter Publish Date:" id="pub_date" {...register('pdate', { required: true })} />
                         <input className="input-content" type="text" placeholder="Enter tags: (Separated using a comma)" id="tags" {...register('tags', { required: true })} />
-                        <input className="submit-btn" type="submit" />
+                        <input className="submit-btn mt-5" type="submit" />
                     </form>
                 </div>
             </div>
@@ -72,6 +76,7 @@ const AddBooks = () => {
                             <div class="Scriptcontent">
                                 <div id='cardss' class="animated fadeIn">
                                 <div id='upper-side' className="success">
+                                <img src={Checked}/>
                                     <h3 id='status'>
                                     Success
                                     </h3>
@@ -92,6 +97,7 @@ const AddBooks = () => {
                             <div class="Scriptcontent">
                                 <div id='cardss' class="animated fadeIn">
                                 <div id='upper-side' className="failed">
+                                <img src={Cancel}/>
                                     <h3 id='status'>
                                     Failed
                                     </h3>
@@ -100,6 +106,9 @@ const AddBooks = () => {
                                     <p id='message'>
                                     Sorry, your data could not be posted correctly. This maybe be caused due to incomplete or incorrect form entries.
                                     </p>
+                                    {respStatus ? (
+                                        <p id="message"><b>Reason: {respStatus}</b></p>
+                                    ) : ''}
                                     <a href="/addBooks" id="contBtn" className="failed">Continue</a>
                                 </div>
                                 </div>
